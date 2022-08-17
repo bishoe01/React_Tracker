f189d6e8e9da40f7dd52763b0d7bafc39a32e63c
bishoe01 <bishoe01@gmail.com>
2022-08-17 22:06:55 +0900

    use utube api

--- youtube/src/app.jsx
+++ youtube/src/app.jsx
@@ -1,8 +1,24 @@
+import { useEffect, useState } from 'react';
 import './app.css';
+import Header from './components/header';
 
 function App() {
+  const [videos, setvideos] = useState([]);
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDBQGxHj3x3XkCYaNQgvvgzIqDulJjY_cI", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+  },[]);
   return (
+    <>
+    <Header/>
     <h1>Hello :)</h1>
+    </>
   );
 }
 
---
+++ youtube/src/components/header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+function Header() {
+    return (
+        <nav>
+            YOUTUBE
+        </nav>
+    )}
+
+
+export default Header;
