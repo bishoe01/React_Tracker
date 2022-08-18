37421278de31eb9dce0c0fdca689f5d1d94db3cc
bishoe01 <bishoe01@gmail.com>
2022-08-19 00:15:38 +0900

    search function

--- youtube/src/app.jsx
+++ youtube/src/app.jsx
@@ -5,8 +5,17 @@ import VideoList from './components/video_list/video_list';
 
 function App() {
   const [videos, setVideos] = useState([]);
-  const [word, setWord] = useState("");
-  
+  const search = (query) => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyDBQGxHj3x3XkCYaNQgvvgzIqDulJjY_cI`, requestOptions)
+      .then(response => response.json())
+      .then(result => result.items.map(item => ({...item , id : item.id.videoId})))
+      .then(items => setVideos(items))
+      .catch(error => console.log('error', error));
+  }
   useEffect(() => {
     const requestOptions = {
       method: 'GET',
@@ -16,25 +25,16 @@ function App() {
       .then(response => response.json())
       .then(result => setVideos(result.items))
       .catch(error => console.log('error', error));
-  },[]);
+  }, []);
 
 
-  // useEffect(() => {
-  //     const requestOptions = {
-  //       method: 'GET',
-  //       redirect: 'follow'
-  //     };
-  //     fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=bts&key=AIzaSyDBQGxHj3x3XkCYaNQgvvgzIqDulJjY_cI", requestOptions)
-  //       .then(response => response.json())
-  //       .then(result => setVideos(result.items))
-  //       .catch(error => console.log('error', error));
-  //   },[word]);
-  
-  
+
+
+
   return (
-    <div  className={styles.app}>
-    <Header word={word}/>
-    <VideoList videos={videos} />
+    <div className={styles.app}>
+      <Header onSearch={search} />
+      <VideoList videos={videos} />
     </div>
   );
 }
--- youtube/src/components/header/header.jsx
+++ youtube/src/components/header/header.jsx
@@ -1,24 +1,29 @@
 import React, { useEffect, useRef, useState } from 'react';
 import styles from './header.module.css';
-function Header(props) {
+function Header({onSearch}) {
 
-    const formRef = React.createRef();
-    const inputRef = React.createRef();
+    const inputRef = useRef();
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        props.setWord(inputRef.current.value);
-
+    const handleSearch = () => {
+        const value = inputRef.current.value;
+        onSearch(value);
     }
-
+    const onKeyPress = (event) => {
+        if(event.key === 'Enter'){
+            handleSearch();
+        };
+    }
+    const onClick = () => {
+        handleSearch();
+    };
     return (
         <header className={styles.header}>
             <div className={styles.logo}>
-            <img className={styles.img} src="/images/logo.png" alt="logo" />
-            <h1 className={styles.title}>Youtube</h1>
+                <img className={styles.img} src="/images/logo.png" alt="logo" />
+                <h1 className={styles.title}>Youtube</h1>
             </div>
-            <input className={styles.input} type="search" placeholder='Search...'/>
-            <button className={styles.button} type="submit" >
+            <input ref={inputRef} className={styles.input} type="search" placeholder='Search...' onKeyPress={onKeyPress} />
+            <button className={styles.button} type="submit" onClick={onClick} >
                 <img className={styles.buttonImg} src="images/search.png" alt="search" />
 
             </button>
