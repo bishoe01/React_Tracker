2a6c45f37ff3390430b3e7747b6d5161c924fa11
bishoe01 <bishoe01@gmail.com>
2022-08-19 10:56:21 +0900

    add promist component

--- youtube/src/app.jsx
+++ youtube/src/app.jsx
@@ -3,34 +3,19 @@ import styles from './app.module.css';
 import Header from './components/header/header';
 import VideoList from './components/video_list/video_list';
 
-function App() {
+function App({youtube}) {
   const [videos, setVideos] = useState([]);
+
   const search = (query) => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyDBQGxHj3x3XkCYaNQgvvgzIqDulJjY_cI`, requestOptions)
-      .then(response => response.json())
-      .then(result => result.items.map(item => ({...item , id : item.id.videoId})))
-      .then(items => setVideos(items))
-      .catch(error => console.log('error', error));
+    youtube
+    .search(query)
+    .then(videos => setVideos(videos));
   }
   useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDBQGxHj3x3XkCYaNQgvvgzIqDulJjY_cI", requestOptions)
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
-  }, []);
-
-
-
-
-
+    youtube
+    .mostPopular()
+    .then(videos => setVideos(videos));
+  },[]);
   return (
     <div className={styles.app}>
       <Header onSearch={search} />
--- youtube/src/index.js
+++ youtube/src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app';
+import Youtube from './components/service/youtube';
 
+const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <App youtube={youtube}/>
 );
